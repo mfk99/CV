2b5de0e941cb4658e3f7996287597f4dbbdbb90a
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-06-15 22:26:02 +0300

    add text elements to education

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -7,15 +7,15 @@ export function Education() {
         <b style={{ color: "#bdbdbd" }}>MSc Computer Science </b>
         <br />
         Algortihms-track <br />
-        Sept. 2023 - Dec. 2026
+        <a>Sept. 2023 - Dec. 2026</a>
         <br />
-        University of Helsinki
+        <a>University of Helsinki</a>
         <br />
         <br />
         <b>BSc Computer Science</b> <br />
-        Sept. 2019 - May 2023
+        <a>Sept. 2019 - May 2023</a>
         <br />
-        University of Helsinki
+        <a>University of Helsinki</a>
       </div>
     </>
   );
